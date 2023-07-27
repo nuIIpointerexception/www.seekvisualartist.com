d98183551d758c015ddce9684c6a82f031bda5f6
seek <61627919+nuIIpointerexception@users.noreply.github.com>
2023-07-27 21:27:48 +0200

    mobile fix?

--- app/components/background/ContactBackground.tsx
+++ app/components/background/ContactBackground.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ContactBackground: React.FC = () => {
     return (
         <div className="absolute inset-0">
-            <video className="h-full w-full object-cover" autoPlay={true} loop muted preload="auto">
+            <video className="h-full w-full object-cover" autoPlay={true} loop playsInline={true} muted preload="auto">
                 <source src="/contact.webm" type="video/webm" />
             </video>
             <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-bg-dark to-transparent"/>
--- app/components/background/HeroBackground.tsx
+++ app/components/background/HeroBackground.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroBackground: React.FC = () => {
     return (
         <div className="absolute inset-0">
-            <video className="h-full w-full object-cover" autoPlay={true} loop muted preload="auto">
+            <video className="h-full w-full object-cover" autoPlay={true} playsInline={true} loop muted preload="auto">
                 <source src="/hero.webm" type="video/webm" />
             </video>
             <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-bg-dark to-transparent"/>
