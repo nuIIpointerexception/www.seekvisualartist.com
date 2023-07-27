6a5b0ee1528bbd6077ab848a270053eb34aefc10
seek <61627919+nuIIpointerexception@users.noreply.github.com>
2023-07-27 17:11:06 +0200

    recompile, bugged idk why?

--- app/components/background/ContactBackground.tsx
+++ app/components/background/ContactBackground.tsx
@@ -6,8 +6,8 @@ const ContactBackground: React.FC = () => {
             <video className="absolute inset-0 h-full w-full object-cover" autoPlay loop muted playsInline preload="auto">
                 <source src="/contact.webm" type="video/webm" />
             </video>
-            <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-bg-dark to-transparent"></div>
-            <div className="absolute inset-0 w-full h-full bg-gradient-to-b from-bg-dark to-transparent"></div>
+            <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-bg-dark to-transparent"/>
+            <div className="absolute inset-0 w-full h-full bg-gradient-to-b from-bg-dark to-transparent"/>
         </div>
     );
 };
